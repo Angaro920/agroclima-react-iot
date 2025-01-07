809a2555d7041e0c9f4fec4a9e3d8dbacfb6c7af
Angaro <diego_angarita@hotmail.com>
2025-01-07 17:07:25 -0500

    left side menu 0.1v

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState  } from 'react'
+import { Layout } from 'antd'
+import {siderStyle} from './styles.tsx'
+import { MenuDashboard } from './components'
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Header, Content, Footer, Sider } = Layout;
 
+
+const App = () => {
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout>
+     <Header>
+      Este es el header
+     </Header>
+     <Layout>
+      <Sider style={siderStyle}>
+        <MenuDashboard />
+      </Sider>
+      <Content>Aca va el contenido</Content>
+     </Layout>
+     <Footer>Este es el footer</Footer>
+    </Layout>
   )
 }
 
